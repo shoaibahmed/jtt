41d9163d33380631812d7e87282803f21cfa836c
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-04-20 15:04:21 +0100

    Different upweighting factors added

--- train.sh
+++ train.sh
@@ -6,6 +6,8 @@ bash results/CUB/CUB_sample_exp/ERM_upweight_0_epochs_300_lr_1e-05_weight_decay_
 
 python process_training.py --exp_name CUB_sample_exp --dataset CUB --folder_name ERM_upweight_0_epochs_300_lr_1e-05_weight_decay_1.0 --lr 1e-05 --weight_decay 1.0 --final_epoch 60 --deploy
 
-bash results/CUB/CUB_sample_exp/train_downstream_ERM_upweight_0_epochs_300_lr_1e-05_weight_decay_1.0/final_epoch60/JTT_upweight_100_epochs_300_lr_1e-05_weight_decay_1.0/job.sh
+for upweight in 20 50 100; do
+    bash results/CUB/CUB_sample_exp/train_downstream_ERM_upweight_0_epochs_300_lr_1e-05_weight_decay_1.0/final_epoch60/JTT_upweight_${upweight}_epochs_300_lr_1e-05_weight_decay_1.0/job.sh
+done
 
 python analysis.py --exp_name CUB_sample_exp/train_downstream_ERM_upweight_0_epochs_300_lr_1e-05_weight_decay_1.0/final_epoch60/ --dataset CUB
