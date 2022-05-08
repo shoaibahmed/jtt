6e794f55e7238a2858f5803e7e0339ea817d67f9
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-05-08 04:43:37 +0200

    Final waterbirds scripts added

--- waterbirds_paper_reprod.sh
+++ waterbirds_paper_reprod.sh
@@ -6,6 +6,14 @@
 # JTT, this yields learning rate 1e-5 and l2 regularization 1.
 # Grid search: JTT yields T = 60 epochs and λup = 100
 
+# job_name=waterbirds-paper-reprod
+# srun -p A100 -K -N1 --ntasks-per-node=1 --gpus-per-task=1 --cpus-per-gpu=6 --mem=50G \
+#     --kill-on-bad-exit --job-name ${job_name} --nice=0 --time 3-00:00:00 \
+#     --container-mounts=/netscratch:/netscratch,/ds:/ds,/home/siddiqui:/home/siddiqui --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_21.06-py3.sqsh \
+#     --container-workdir=`pwd` --container-mount-home --export="NCCL_SOCKET_IFNAME=bond,NCCL_IB_HCA=mlx5" \
+#     ./waterbirds_paper_reprod.sh > ./logs/${job_name}.log 2>&1 &
+
+export WANDB_MODE=offline
 
 python generate_downstream.py --exp_name CUB_reproduce_paper --dataset CUB --n_epochs 300 --lr 1e-5 --weight_decay 1.0 --method ERM
 
